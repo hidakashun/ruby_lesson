325986851691e4a085e9fd2dd8bcb62ef4d4e7e7
hidakashun <hidakashun60@gmail.com>
2023-11-20 05:39:34 +0000

    「eachメソッド」は、オブジェクト内の要素を順に取り出すメソッド

---
+++ amounts.rb
@@ -0,0 +1,6 @@
+amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+amounts.each do |fruit, amount|  #ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
+  puts "#{fruit}は#{amount}個です。"
+end
+
+#eachメソッドで記述することで、データの先頭から順に繰り返して処理が行われます。
